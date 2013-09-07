c9401a4044fbf636fb1b6a367748c1d84633b17e
Oliver Budiardjo <oliver@budiardjo.com>
2013-09-07 16:45:01 -0400

    project

---
+++ Documents/workspace/Android_Development/tenThousand_hour_project/src/com/example/tenthousand_hour_project/DatabaseHelper.java
@@ -0,0 +1,35 @@
+package com.example.tenthousand_hour_project;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class Database extends SQLiteOpenHelper {
+	 
+    final static int DB_VERSION = 1;
+    final static String DB_NAME = "StopWatch_database";
+    Context context;
+     
+    public Database(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        // Store the context for later use
+        this.context = context;
+    }
+
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		arg0.execSQL("CREATE TABLE user_table(userName TEXT, password TEXT, user_id int, active int)");
+		arg0.execSQL("CREATE TABLE activity_table(user_id int, activity_name TEXT, activity_time real)");
+		
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		arg0.execSQL("DROP TABLE IF EXISTS user_table");
+		arg0.execSQL("DROP TABLE IF EXISTS activity_table");
+
+		onCreate(arg0);
+	}
+	
+}
---
+++ Documents/workspace/Android_Development/tenThousand_hour_project/src/com/example/tenthousand_hour_project/Login.java
@@ -0,0 +1,91 @@
+package com.example.tenthousand_hour_project;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends Activity {
+
+	Button SubmitButton;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		SubmitButton = (Button) findViewById(R.id.login_button);
+		SubmitButton.setOnClickListener(SubmitListener);
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+	
+	private OnClickListener SubmitListener = new OnClickListener() {
+	    public void onClick(View v) {
+	    	SQLiteDatabase qdb = openDB();
+	    	EditText name_text = (EditText) findViewById(R.id.name_box);
+			EditText password_text = (EditText) findViewById(R.id.password_box);
+			String name = name_text.getText().toString();
+			String password = password_text.getText().toString();
+
+			Cursor nameQuery = qdb.rawQuery("SELECT password FROM user_table WHERE userName = '" + name + "'", null);
+			if (nameQuery.getCount() == 0){
+				ContentValues newUser = new ContentValues();
+				newUser.put("userName", name);
+				newUser.put("password", password);
+				newUser.put("active", 1);
+				int newId = findNewId(qdb);
+				newUser.put("user_id", newId);
+				long newRowId;
+				newRowId = qdb.insert("user_table", null, newUser);
+				switchActivity();
+			}
+			else{
+				if (nameQuery.toString() == password){
+					ContentValues userContent = new ContentValues();
+					userContent.put("active", 1);
+					long rowId;
+					rowId = qdb.update("user_table", userContent, "user_name=" + name, null);
+					switchActivity();
+				}
+			
+			
+			}
+			
+	    }
+	};
+	
+	private SQLiteDatabase openDB(){
+		Database db = new Database(this);
+		SQLiteDatabase qdb = db.getWritableDatabase();
+		return qdb;
+  	
+	}
+	
+	public void switchActivity(){
+		Intent intent = new Intent(this, List.class);
+		startActivity(intent);	    
+	}
+	
+	public int findNewId(SQLiteDatabase qdb){
+		Cursor idQuery = qdb.rawQuery("SELECT user_id FROM user_table", null);
+		idQuery.moveToLast();
+		int lastIdInt = idQuery.getInt(idQuery.getColumnIndex("user_id"));
+		return lastIdInt + 1;
+	}
+	
+
+}
