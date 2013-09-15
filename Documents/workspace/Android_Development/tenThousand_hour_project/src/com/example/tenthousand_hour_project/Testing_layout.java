c9c5697f0896831a25d7f3c2ad885178f4ef63e2
Oliver Budiardjo <oliver@budiardjo.com>
2013-09-15 14:26:45 -0400

    Revert "The beginnings of a stopwatch."
    This reverts commit 0494dec87e7ea3c6a9aaf4909f744f3fb0f58fc3.

--- Documents/workspace/Android_Development/tenThousand_hour_project/src/com/example/tenthousand_hour_project/Testing_layout.java
+++ Documents/workspace/Android_Development/tenThousand_hour_project/src/com/example/tenthousand_hour_project/Testing_layout.java
@@ -4,21 +4,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class Stopwatch extends Activity {
+public class Testing_layout extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_stopwatch);
+		setContentView(R.layout.activity_testing_layout);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.stopwatch, menu);
+		getMenuInflater().inflate(R.menu.testing_layout, menu);
 		return true;
 	}
 
-	
-	
 }
