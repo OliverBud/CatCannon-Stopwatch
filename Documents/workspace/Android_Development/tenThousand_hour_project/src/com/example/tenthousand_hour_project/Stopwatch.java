b92fe997b82b5d00ff00a5352f72a36fc903079d
Oliver Budiardjo <oliver@budiardjo.com>
2013-09-15 14:27:28 -0400

    Revert "Revert "The beginnings of a stopwatch.""
    This reverts commit c9c5697f0896831a25d7f3c2ad885178f4ef63e2.

--- Documents/workspace/Android_Development/tenThousand_hour_project/src/com/example/tenthousand_hour_project/Stopwatch.java
+++ Documents/workspace/Android_Development/tenThousand_hour_project/src/com/example/tenthousand_hour_project/Stopwatch.java
@@ -1,15 +1,72 @@
 package com.example.tenthousand_hour_project;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Chronometer;
 
 public class Stopwatch extends Activity {
 
+	Button start_button = (Button) findViewById(R.id.start_button);
+	Button stop_button = (Button) findViewById(R.id.stop_button);
+	TextView small_text = (TextView) findViewById(R.id.small_text);
+	Chronometer timekeeper = (Chronometer) findViewById(R.id.timekeeper);
+	
+	//long timecreate;
+	long elapsed;
+	String activity_name;
+	String arctive_activity_name;
+	SQLiteDatabase qdb;
+	Cursor timespot;
+	
+	private OnClickListener start_listener = new OnClickListener() {
+		public void onClick(View v) {
+			timekeeper.setBase(0);
+			timekeeper.start();
+		}
+	};
+	
+	private OnClickListener stop_listener = new OnClickListener() {
+		public void onClick(View v) {
+			timekeeper.stop();
+			elapsed = timekeeper.getBase();
+			
+			timespot = null;
+			timespot = qdb.rawQuery("SELECT activity_time FROM activity_table WHERE " +
+					"activity_name = '" + activity_name + "'", null);
+			timespot.moveToFirst();
+			
+			long the_time = timespot.getLong(timespot.getColumnIndexOrThrow("activity_time"));
+			ContentValues update_time = new ContentValues();
+			update_time.put("activity_time", the_time + elapsed);
+		}
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_stopwatch);
+		start_button.setOnClickListener(start_listener);
+		stop_button.setOnClickListener(stop_listener);
+		
+		Database db = new Database(this);
+		qdb = db.getWritableDatabase();
+		
+		Cursor active_activity = qdb.rawQuery("SELECT activity_name FROM activity_table " +
+				"WHERE active = 1", null);
+		activity_name = active_activity.getString(active_activity.getColumnIndexOrThrow(
+				"activity_name"));
+		
+		
+		
 	}
 
 	@Override
